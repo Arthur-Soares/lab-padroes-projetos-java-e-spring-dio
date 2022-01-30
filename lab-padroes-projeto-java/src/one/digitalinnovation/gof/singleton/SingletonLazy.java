66117b3f9038a5c63ee722ae14121307b2e0a44d
Arthur-Soares <asoaresdasilva@outlook.com>
2022-01-30 17:24:05 -0300

    First Commit padrões projetos dio - java e spring

---
+++ lab-padroes-projeto-java/src/one/digitalinnovation/gof/singleton/SingletonLazy.java
@@ -0,0 +1,25 @@
+package one.digitalinnovation.gof.singleton;
+
+/**
+ * Singleton "preguiçoso".
+ * 
+ * @author Arthur-Soares
+ */
+
+public class SingletonLazy {
+
+	private static SingletonLazy instancia;
+	
+    private SingletonLazy() {
+		super();
+	}
+
+	public static SingletonLazy getInstancia() {
+		if (instancia == null) {
+			instancia = new SingletonLazy();
+		}
+		return instancia;
+	}
+    
+    
+}
